28a9e8fa39af6269a415eb6167fbcbbb14ddb2ce
MikeRodziewicz <michal.rodziewicz1@gmail.com>
2020-05-08 17:17:44 +0200

    DB connection working with Postgres

--- backend.py
+++ backend.py
@@ -4,3 +4,35 @@
 #  Could also be made into a shout box.
 #  *Optional: Deploy it on Google App Engine or Heroku or any other PaaS
 #  (if possible, of course).*
+
+
+import psycopg2
+from datetime import datetime
+
+
+def connect():
+    conn = psycopg2.connect(
+        "dbname='GuestBook' user='postgres' password='password123' host='localhost' port='5432'")
+    cur = conn.cursor()
+    cur.execute(
+        "CREATE TABLE IF NOT EXISTS guests (name TEXT, comment TEXT, date TIMESTAMP)")
+    conn.commit()
+    conn.close()
+
+
+def insert(name, comment, date):
+    conn = psycopg2.connect(
+        "dbname='GuestBook' user='postgres' password='password123' host='localhost' port='5432'")
+    cur = conn.cursor()
+    cur.execute("INSERT INTO guests VALUES (%s,%s,%s)", (name, comment, date))
+    conn.commit()
+    conn.close()
+
+ def view():
+    conn = psycopg2.connect(
+        "dbname='GuestBook' user='postgres' password='password123' host='localhost' port='5432'")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM guests")
+    rows = cur.fetchall()
+    conn.close()
+    return} rows
--- guestbook.py
+++ guestbook.py
@@ -1,1 +1,14 @@
-print("hello world")
+# **Guestbook / Journal** - A simple application that allows people to add
+# comments or write journal entries. It give a timestamp to each entry and stores
+# it in postrgesSQL.
+# Once you make an entry - e-mail confirmation gets send to you with
+# all your previous comments.
+# Deploy to the live server or setup with LAMP on Raspi.
+
+
+from datetime import datetime
+import backend
+
+
+if __name__ == "__main__":
+    data = datetime.now()
