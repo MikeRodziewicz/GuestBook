0783809c1d981d138d5a5254930bd80f6cf9bed4
MikeRodziewicz <michal.rodziewicz1@gmail.com>
2020-05-08 22:23:20 +0200

    Flask working, templates rendering, no JS functionality

--- backend.py
+++ backend.py
@@ -1,11 +1,3 @@
-# **Guestbook / Journal** - A simple application that allows people to add
-# comments or write journal entries. It can allow comments or
-# not and timestamps for all entries.
-#  Could also be made into a shout box.
-#  *Optional: Deploy it on Google App Engine or Heroku or any other PaaS
-#  (if possible, of course).*
-
-
 import psycopg2
 from datetime import datetime
 
@@ -28,11 +20,12 @@ def insert(name, comment, date):
     conn.commit()
     conn.close()
 
- def view():
+
+def view():
     conn = psycopg2.connect(
         "dbname='GuestBook' user='postgres' password='password123' host='localhost' port='5432'")
     cur = conn.cursor()
     cur.execute("SELECT * FROM guests")
     rows = cur.fetchall()
     conn.close()
-    return} rows
+    return rows
--- guestbook.py
+++ guestbook.py
@@ -8,7 +8,23 @@
 
 from datetime import datetime
 import backend
+from flask import Flask, render_template
+
+
+# data = datetime.now()
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+
+@app.route('/sukces/')
+def sukces():
+    return render_template("sukces.html")
 
 
 if __name__ == "__main__":
-    data = datetime.now()
+    app.run(debug=True)
